d2bf05efd27e7b02f969b96aa45cae8d90bb6236
kakadepratik7 <91534775+kakadepratik7@users.noreply.github.com>
2021-09-28 16:46:32 +0530

    uploaded assignment file

---
+++ Que1.java
@@ -0,0 +1,10 @@
+package assignments;
+//Question no. 1 Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+public class Que1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello\nPratik Kakade");
+
+	}
+
+}
